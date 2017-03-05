73863f4df5c63caa8f7a958557e830416bfd2520
aprilr4 <aprilr4@uw.edu>
2017-03-04 20:14:11 -0800

    Images and Initial Javascript

---
+++ js/dimensions.js
@@ -0,0 +1,296 @@
+var DIMENSIONS = [
+    {
+        "title":  "Starter Pack Batman Gandalf Wildstyle Batmobile PS3",
+        "number": 71170,
+        "type": "Starter Pack",
+        "characters": "Batman, Gandalf, Wildstyle",
+        "image": "img/71170.jpg"
+    },
+    {
+        "title": "Back to the Future Level Pack",
+        "number": 71201,
+        "type": "Level Pack",
+        "characters": "Marty McFly",
+        "image": "img/71201.jpg"
+    },
+    {
+        "title": "The Simpsons Level Pack",
+        "number": 71202,
+        "type": "Level Pack",
+        "characters": "Homer Simpson",
+        "image": "img/71202.jpg"
+    },
+    {
+        "title": "Portal Level Pack",
+        "number": 71203,
+        "type": "Level Pack",
+        "characters": "Chell, Companion Cube, Turret",
+        "image": "img/71203.jpg" 
+    },
+    {
+        "title": "Doctor Who Level Pack",
+        "number": 71204,
+        "type": "Level Pack",
+        "characters": "The Doctor, K9",
+        "image": "img/71204.jpg"
+    },
+    {
+        "title": "Jurrasic World Team Pack",
+        "number": 71205,
+        "type": "Team Pack",
+        "characters": "Owen, ACU",
+        "image": "img/71205.jpg"
+    },
+    {
+        "title": "Scooby Doo! Team Pack",
+        "number": 71206,
+        "type": "Team Pack",
+        "characters": "Shaggy, Scooby",
+        "image": "img/71206.jpg"
+    },
+    {
+        "title": "Wonder Woman Fun Pack",
+        "number": 71209,
+        "type": "Fun Pack",
+        "characters": "Wonder Woman",
+        "image": "img/71209.jpg"
+    },
+        {
+        "title": "Cyborg Fun Pack",
+        "number": 71210,
+        "type": "Fun Pack",
+        "characters": "Cyborg",
+        "image": "img/71210.jpg"
+    },
+    {
+        "title": "Bart Fun Pack",
+        "number": 71211,
+        "type": "Fun Pack",
+        "characters": "Bart",
+        "image": "img/71211.jpg"
+    },
+    {
+        "title": "Emmet Fun Pack",
+        "number": 71212,
+        "type": "Fun Pack",
+        "characters": "Emmet",
+        "image": "img/71212.jpg"
+    },
+    {
+        "title": "Bad Cop Fun Pack",
+        "number": 71213,
+        "type": "Fun Pack",
+        "characters": "Bad Cop",
+        "image": "img/71213.jpg"
+    },
+        {
+        "title": "Benny Fun Pack",
+        "number": 71214,
+        "type": "Fun Pack",
+        "characters": "Benny",
+        "image": "img/71214.jpg"
+    },
+    {
+        "title": "Nya Fun Pack",
+        "number": 71216,
+        "type": "Fun Pack",
+        "characters": "Nya",
+        "image": "img/71216.jpg"
+    },
+    {
+        "title": "Zane Fun Pack",
+        "number": 71217,
+        "type": "Fun Pack",
+        "characters": "Zane",
+        "image": "img/71217.jpg"
+    },
+    {
+        "title": "Gollum Fun Pack",
+        "number": 71218,
+        "type": "Fun Pack",
+        "characters": "Gollum, Shelob the Great",
+        "image": "img/71218.jpg"
+    },
+        {
+        "title": "Legolas Fun Pack",
+        "number": 71219,
+        "type": "Fun Pack",
+        "characters": "Legolas",
+        "image": "img/71219.jpg"
+    },
+    {
+        "title": "Gimli Fun Pack",
+        "number": 71220,
+        "type": "Fun Pack",
+        "characters": "Gimli",
+        "image": "img/71220.jpg"
+    },
+    {
+        "title": "Wicked Witch Fun Pack",
+        "number": 71221,
+        "type": "Fun Pack",
+        "characters": "Wicked Witch (Oz)",
+        "image": "img/71221.jpg"
+    },
+    {
+        "title": "Laval Fun Pack",
+        "number": 71222,
+        "type": "Fun Pack",
+        "characters": "Laval",
+        "image": "img/71222.jpg"
+    },
+        {
+        "title": "Cragger Fun Pack",
+        "number": 71223,
+        "type": "Fun Pack",
+        "characters": "Cragger",
+        "image": "img/71223.jpg"
+    },
+    {
+        "title": "Krusty Fun Pack",
+        "number": 71227,
+        "type": "Fun Pack",
+        "characters": "Krusty the Clown",
+        "image": "img/71227.jpg"
+    },
+    {
+        "title": "Ghostbusters Level Pack",
+        "number": 71228,
+        "type": "Level Pack",
+        "characters": "Peter Venkman",
+        "image": "img/71228.jpg"
+    },
+    {
+        "title": "DC Comics Team Pack",
+        "number": 71229,
+        "type": "Team Pack",
+        "characters": "Joker, Harley Quinn",
+        "image": "img/71229.jpg"
+    },
+        {
+        "title": "Doc Brown Fun Pack",
+        "number": 71230,
+        "type": "Fun Pack",
+        "characters": "Doc Brown",
+        "image": "img/71230.jpg"
+    },
+    {
+        "title": "Unikitty Fun Pack",
+        "number": 71231,
+        "type": "Fun Pack",
+        "characters": "Unikitty",
+        "image": "img/71231.jpg"
+    },
+    {
+        "title": "Eris Fun Pack",
+        "number": 71232,
+        "type": "Fun Pack",
+        "characters": "Eris",
+        "image": "img/71232.jpg"
+    },
+    {
+        "title": "Stay Puft Fun Pack",
+        "number": 71233,
+        "type": "Fun Pack",
+        "characters": "Stay Puft Marshmallow Man",
+        "image": "img/71233.jpg"
+    },
+        {
+        "title": "Sensei Wu Fun Pack",
+        "number": 71234,
+        "type": "Fun Pack",
+        "characters": "Sensei Wu",
+        "image": "img/71234.jpg"
+    },
+    {
+        "title": "Midway Arcade Level Pack",
+        "number": 71235,
+        "type": "Level Pack",
+        "characters": "Gamer Kid",
+        "image": "img/71235.jpg"
+    },
+    {
+        "title": "Aquaman Fun Pack",
+        "number": 71237,
+        "type": "Fun Pack",
+        "characters": "Aquaman",
+        "image": "img/71237.jpg"
+    },
+    {
+        "title": "Cyberman Fun Pack",
+        "number": 71238,
+        "type": "Fun Pack",
+        "characters": "Cyberman, Dalek",
+        "image": "img/71238.jpg"
+    },
+    {
+        "title": "Slimer Fun Pack",
+        "number": 71241,
+        "type": "Fun Pack",
+        "characters": "Slimer",
+        "image": "img/71241.jpg"
+    },
+        {
+        "title": "Ghostbusters Story Pack",
+        "number": 71242,
+        "type": "Story Pack",
+        "characters": "Abby Yates",
+        "image": "img/71242.jpg"
+    },
+    {
+        "title": "Adventure Time Level Pack",
+        "number": 71245,
+        "type": "Level Pack",
+        "characters": "Finn the Human",
+        "image": "img/71245.jpg"
+    },
+    {
+        "title": "Harry Potter Team Pack",
+        "number": 71247,
+        "type": "Team Pack",
+        "characters": "Harry Potter, Voldemort",
+        "image": "img/71247.jpg"
+    },
+    {
+        "title": "Mission Impossible Level Pack",
+        "number": 71248,
+        "type": "Level Pack",
+        "characters": "Ethan Hunt",
+        "image": "img/71248.jpg"
+    },
+        {
+        "title": "A-Team Fun Pack",
+        "number": 71251,
+        "type": "Fun Pack",
+        "characters": "B.A. Baracus",
+        "image": "img/71251.jpg"
+    },
+    {
+        "title": "Fantastic Beasts and Where to Find Them Story Pack",
+        "number": 71253,
+        "type": "Story Pack",
+        "characters": "Newt Scamander, Niffler",
+        "image": "img/71253.jpg"
+    },
+    {
+        "title": "Fantastic Beasts and Where to Find Them Fun Pack",
+        "number": 71257,
+        "type": "Fun Pack",
+        "characters": "Tina Goldstein, Swooping Evil",
+        "image": "img/71257.jpg"
+    },
+    {
+        "title": "Green Arrow",
+        "number": 71342,
+        "type": "Limited Edition",
+        "characters": "Green Arrow",
+        "image": "img/71342.jpg"
+    },
+    {
+        "title": "LEGO Batman Movie Story Pack",
+        "number": 71264,
+        "type": "Story Pack",
+        "characters": "Robin, Batgirl",
+        "image": "img/71264.jpg"
+    },
+];
